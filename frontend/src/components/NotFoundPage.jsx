7d92f17fc572fc08c0103662a5dc14c55098c0e3
sobolevaea <liziaka@mail.ru>
2025-11-17 18:06:17 +0300

    fix: update 404 page (Not found)

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,7 +1,19 @@
 const NotFoundPage = () => (
-  <>
-    <h3>404 ERROR</h3>
-  </>
+  <div className="h-100">
+    <div className="d-flex flex-column h-100">
+      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container">
+          <a className="navbar-brand" href="/">Hexlet Chat</a>
+          <button type="button" className="btn btn-primary">Выйти</button>
+        </div>
+      </nav>
+      <div className="text-center">
+        <img alt="Страница не найдена" className="img-fluid h-25" src="./src/assets/404.svg" />
+        <h1 className="h4 text-muted">Страница не найдена</h1>
+        <p className="text-muted">Но вы можете перейти <a href="/">на главную страницу</a></p>
+      </div>
+    </div>
+  </div>
 )
 
 export default NotFoundPage
